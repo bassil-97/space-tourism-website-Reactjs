db6043be67f666efde733ab73eac475a46cda30b
bassil-97 <61696257+bassil-97@users.noreply.github.com>
2022-02-11 21:31:52 +0200

    Add logo link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,9 +8,9 @@ import logo from '../../assets/shared/logo.svg';
 export default function Navbar() {
   return( 
     <nav className="navbar navbar-expand-lg navbar-dark">
-        <a className="navbar-brand" href="#">
+        <Link to="/home" className="navbar-brand">
             <img src={logo} alt="Logo" />
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
